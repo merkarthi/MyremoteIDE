9505e852941176b462dd25ff0305d398e0ca39d8
Karthick R <karthickr.kar@hcl.com>
2019-11-05 05:55:35 +0000

    Class

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 class Person:
-    def __init__()
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
 
-    
+    def say_hi(merk):
+        print("My name is ",merk.name,"\nI'm ",merk.age,"year old")
+
+p=Person("karthi",15)
+p.say_hi()
+
